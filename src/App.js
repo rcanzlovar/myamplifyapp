e8ef9ac6dabbf5c23fa6322896c9f9a75eb2c849
Bob Anzlovar <rcanzlovar@gmail.com>
2020-02-18 13:23:39 -0700

    another incremental commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is how I will Learn React
         </a>
       </header>
     </div>
